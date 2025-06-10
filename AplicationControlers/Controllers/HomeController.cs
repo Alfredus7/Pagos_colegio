77017c2fd76e98d4f106ed49c7d04554f8f42d5b
Freddy david Avalos pardos <fredav201612@gmail.com>
2025-06-10 10:05:38 -0400

    hora de estilizar

--- AplicationControlers/Controllers/HomeController.cs
+++ AplicationControlers/Controllers/HomeController.cs
@@ -19,11 +19,6 @@ namespace Pagos_colegio.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
